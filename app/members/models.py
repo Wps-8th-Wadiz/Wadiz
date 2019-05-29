40023762ee1576d36adf9aae79e571f5bbe881ac
Ryanden <lockstom@gmail.com>
2019-05-30 01:12:12 +0900

    last update

--- app/members/models.py
+++ app/members/models.py
@@ -1,14 +1,8 @@
-import os
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-from config.settings.base import STATIC_DIR
-
 
 class User(AbstractUser):
-
     img_profile = models.ImageField(
         upload_to='user',
         default='user/blank_user.png'
--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,10 @@ git add -f ./front/fds10-wadiz
 # eb deploy실행
 eb deploy --profile fc-8th-eb --staged
 
+eb ssh && cat /var/log/eb-activity.log
+
 # .secrets 를 제거
 git reset HEAD .secrets
+git reset HEAD ./front/fds10-wadiz
 
 git reset
